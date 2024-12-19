d00e51eaf2d701aaee9b000cc8138b88edec5a6c
wasereter <waser0712@gmail.com>
2024-12-19 14:47:39 +0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,11 @@
+class Employee:
+ pass
+employee = Employee()
+employee.name = 'Max'
+employee.salary = 35000
+employee2 = Employee()
+employee2.name = 'Sergey'
+employee2.salary = 63000 
+print(employee.name)
+print(employee2.name)
+print(employee.salary + employee2.salary)

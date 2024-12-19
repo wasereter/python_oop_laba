89445a9b432cdb96169127fcddf437d5f7ee1596
wasereter <waser0712@gmail.com>
2024-12-19 15:30:34 +0300

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,12 @@
+class Student:
+    name = None
+    surname = None
+class Student:
+    name = 'sergey'
+    surname = 'pribitkov'
+    def show(self):
+        return self.name
+student = Student()
+print(student.name)
+print(student.surname)
+print(student.show())

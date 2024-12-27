e5ea9a517ad649af07e55c2018b4022590acb8ce
wasereter <waser0712@gmail.com>
2024-12-27 13:58:55 +0300

    Create 39.py

---
+++ 39.py
@@ -0,0 +1,24 @@
+class User:
+    def setNickname(self, nick):
+        self.nickname = nick
+        
+    def getNickname(self):
+        return self.nickname
+class Employee(User):
+    def setName(self, name):
+        self.name = name
+        
+    def getName(self):
+        return self.name
+        
+class Programmer(Employee):
+    def setSurn(self, surname):
+        self.surname = surname
+        
+    def getSurn(self):
+        return self.surname
+        
+class Designer(Programmer):
+    def setAge(self, age):
+        self.age = age
+    def getAge(self):

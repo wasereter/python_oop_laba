80f5a538332fe05703087d6eb1620acdf046465d
wasereter <waser0712@gmail.com>
2024-12-27 13:37:18 +0300

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,26 +1,37 @@
 class Employee:
-    __name = None
-    __salary = 0
-    def __init__(self, name, salary):
-        self.__name = name
-        self.__salary = salary
+  __name = None
+  __salary = 0
 
-class EmployeesCollection:
+  def __init__(self,name,salary):
+    self.__name = name
+    self.__salary = salary
+
+  def getName(self):
+    return self.__name
+    
+class EmployeeCollection:
+
     def __init__(self):
         self.__name = []
         self.__salary = []
-    def add(self, name, salary):
-        self.__name.append(name)
-        self.__salary.append(salary)
+
+    def add(self,user, value):
+        self.__name.append(user)
+        self.__salary.append(value)
+
     def show(self):
-        for name in self.__name:
-            print(name)
-        for salary in self.__salary:
-            print(sum(salary))
+        for user in self.__name:
+            print(user)
+      
+    def showSalary(self):
+            print(sum(self.__salary))
     def midsalary(self):
-        print(sum(salary)/len(self.__name))
-uc = EmployeesCollection
-uc.add('john', 242423)
-uc.add('eric', 42121) 
-uc.add('kyle', 31231)
-us.show
+            summa = sum(self.__salary)
+            print(summa/len(self.__name))
+uc = EmployeeCollection()
+uc.add('john', 23215) 
+uc.add('eric', 54784) 
+uc.add('kyle', 62346) 
+uc.show() 
+uc.showSalary()
+uc.midsalary()
